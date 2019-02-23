0e2ae140b24fc7bd6457d275699164425345cc53
Simon Gerritsen <simon4229692@hotmail.com>
2019-02-23 14:51:11 +0100

    Changes for Lesson 2

--- Vuefile.js
+++ Vuefile.js
@@ -4,6 +4,10 @@ var app = new Vue ({
         product: {
             name: 'Socks',
             description: 'A pair of warm, fuzzy socks',
+            image: {
+                path: './assets/vmSocks-green-onWhite.jpg',
+                description: 'Green socks',
+            },
         }
     }
 })
